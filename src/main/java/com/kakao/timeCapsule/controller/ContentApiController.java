6551bd3e03505782eafb1f3240f1bf2868c006d5
kms278711 <kms278711@naver.com>
2022-01-24 14:26:34 +0900

    오류수정

--- src/main/java/com/kakao/timeCapsule/controller/ContentApiController.java
+++ src/main/java/com/kakao/timeCapsule/controller/ContentApiController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-public class PlanApiController {
+public class ContentApiController {
 
     private final BottleService bottleService;
 
